8bfd617693fd1db7522480c6015477c0580eb9f5
alicjakwie <59053998+alicjakwie@users.noreply.github.com>
2020-08-27 12:51:14 +0200

    Update file_gen.go

--- hermes/file_gen.go
+++ hermes/file_gen.go
@@ -1,7 +1,7 @@
-package main
+package hermes
+
 import (
 	"fmt"
-	// "math/rand"
 	"strconv"
 	"crypto/sha1"
 )
@@ -9,20 +9,20 @@ type HermesFile struct {
 	name string
 	contents string
 }
-func generate_file_name (file_ID int, file_checksum string) string{
-	var id_string string=""
+func (file *HermesFile) GenerateFileName(file_ID int, file_checksum string) {
+	id_string :=""
 	if file_ID < 10 {
 		id_string = "0" + strconv.Itoa(file_ID)
 	} else {
 		id_string = strconv.Itoa(file_ID)
 	}
-	var file_name string = "Hermes_"+id_string+file_checksum
-	return file_name 
+	file_name := "Hermes_"+id_string+file_checksum
+	file.name = file_name
 }
-func generate_file_contents (file_ID int) string{
-	return "jhfvjhdfjhfjjhjhdfvjvcvfjh";
+func (file *HermesFile) GenerateFileContents(file_ID int) {
+	file.contents = "jhfvjhdfjhfjjhjhdfvjvcvfjh";
 }
-func generate_file_checksum (file HermesFile) string{
+func (file HermesFile) GenerateFileChecksum() string {
 	
 	file_contents := []byte(file.contents)
 	hash := sha1.Sum(file_contents)
@@ -31,12 +31,8 @@ func generate_file_checksum (file HermesFile) string{
 }
 func generate_file (id int) HermesFile {
 	file := HermesFile{}
-	file.contents = generate_file_contents(id)
-	var checksum string = generate_file_checksum(file)
-	file.name = generate_file_name(id, checksum)
+	file.GenerateFileContents(id)
+	checksum := file.GenerateFileChecksum()
+	file.GenerateFileName(id, checksum)
 	return file
-}
-func main(){
-	var f HermesFile = generate_file(3)
-	fmt.Println(f.name)
 }
