bf70354dc762d98cbed41db129aba42d0af5563b
alicjakwie <59053998+alicjakwie@users.noreply.github.com>
2020-09-01 15:29:14 +0200

    Update file_gen_test.go

--- hermes/file_gen_test.go
+++ hermes/file_gen_test.go
@@ -1,51 +1,37 @@
-// Licensed under the Apache License, Version 2.0 (the "License")
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License. 
- 
-// Author: Alicja Kwiecinska (kwiecinskaa@google.com) github: alicjakwie 
-
-// File generation for Hermes
 package hermes
 
-import (
-	"fmt"
-	"crypto/sha1"
+import(
+	"testing"
+	"strconv"
 )
 
-type HermesFile struct {
-	name string
-	contents string
-}
 
-// method of HermesFile generates HermesFile.name of the form Hermes_id_checksum where id is an integer & id <= 50
-func (file *HermesFile) generateFileName(file_id int, file_checksum string) {
-	file.name = file.name :=  fmt.Sprintf("Hermes_%02d_%v", file_id, file_checksum)
-}
-
-// method of HermesFile generates HermesFile.contents now a string without any significance in the future a pseudo random byte generator will be used
-func (file *HermesFile) generateFileContents(file_id int) {
-	file.contents = "jhfvjhdfjhfjjhjhdfvjvcvfjh";
-}
-
-// method of HermesFile generates the checksum of the file contents
-func (file HermesFile) generateFileChecksum() string {
-	file_contents := []byte(file.contents)
-	hash := sha1.Sum(file_contents)
-	// return checksum in hex notation
-	return fmt.Sprintf("%x", hash)
-}
-
-// method of HermesFile generates the file takes id as a parameter
-func GenerateHermesFile (id int) HermesFile {
+func TestChecksum(t *testing.T){
+	global_contents := "jhfvjhdfjhfjjhjhdfvjvcvfjh"
+	want := "_68f3caf439065824dcf75651c202e9f7c28ebf07" //expected checksum result
 	file := HermesFile{}
-	file.generateFileContents(id)
-	checksum := file.generateFileChecksum()
-	file.generateFileName(id, checksum)
-	return file
+	file.contents = global_contents;
+	got := file.generateFileChecksum()
+	if want != got {
+		t.Errorf("generateFileChecksum() failed expected %v got %v", want, got)
+	}
+}
+
+func TestFileName(t *testing.T){
+	file := HermesFile{}
+	file_ID := 23;
+	fake_checksum :="_abba"
+	want :=  "Hermes_"+strconv.Itoa(file_ID)+"_abba" //expected file name result
+	file.generateFileName(file_ID, fake_checksum)
+	got := file.name
+	if got != want {
+		t.Errorf("generateFileName(%v, \"abba\") failed expected %v got %v", file_ID, want, got)
+	}
+	file_ID = 4;
+	want = "Hermes_0"+strconv.Itoa(file_ID)+"_abba" //expected file name result
+	file.generateFileName(file_ID, fake_checksum)
+	got = file.name
+	if got != want {
+		t.Errorf("generateFileName(%v, \"abba\") failed expected %v got %v", file_ID, want, got)
+	}
 }
