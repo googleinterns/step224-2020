ff60090d28a04d44b2b250f0ca693a9207b8d07d
alicjakwie <59053998+alicjakwie@users.noreply.github.com>
2020-09-02 09:52:05 +0200

    Update file_gen.go

--- hermes/file_gen.go
+++ hermes/file_gen.go
@@ -6,42 +6,43 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License. 
- 
-// Author: Alicja Kwiecinska (kwiecinskaa@google.com) github: alicjakwie 
+// limitations under the License.
+
+// Author: Alicja Kwiecinska (kwiecinskaa@google.com) github: alicjakwie
 
 // File generation for Hermes
 package hermes
 
 import (
-	"fmt"
 	"crypto/sha1"
+	"fmt"
 )
 
 type HermesFile struct {
-	Name string
-	Contents string
+	name     string
+	contents string
 }
 
 // method of HermesFile generates HermesFile.name of the form Hermes_id_checksum where id is an integer & id <= 50
 func (file *HermesFile) generateFileName(file_id int, file_checksum string) {
-	file.name =  fmt.Sprintf("Hermes_%02d_%v", file_id, file_checksum)
+	file.name = fmt.Sprintf("Hermes_%02d_%v", file_id, file_checksum)
 }
 
 // method of HermesFile generates HermesFile.contents now a string without any significance in the future a pseudo random byte generator will be used
 func (file *HermesFile) generateFileContents(file_id int) {
-	file.contents = "jhfvjhdfjhfjjhjhdfvjvcvfjh";
+	file.contents = "jhfvjhdfjhfjjhjhdfvjvcvfjh"
 }
 
 // method of HermesFile generates the checksum of the file contents
 func (file HermesFile) generateFileChecksum() string {
 	file_contents := []byte(file.contents)
 	hash := sha1.Sum(file_contents)
+	// return checksum in hex notation
 	return fmt.Sprintf("%x", hash)
 }
 
 // method of HermesFile generates the file takes id as a parameter
-func GenerateHermesFile (id int) HermesFile {
+func GenerateHermesFile(id int) HermesFile {
 	file := HermesFile{}
 	file.generateFileContents(id)
 	checksum := file.generateFileChecksum()
