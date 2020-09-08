6c4c2076a721aad1146099fe56dbcf727d37a1a9
Evan Spendlove <espendlove@google.com>
2020-09-08 11:38:16 +0000

    Added probe metrics

--- gcs/deleteFile/deleteFile.go
+++ gcs/deleteFile/deleteFile.go
@@ -21,7 +21,7 @@
 // TODO: Setup labels and metrics within probe
 // TODO: Add handling of additional labels
 
-package gcsDelete
+package gcs
 
 import (
 	"context"
@@ -29,36 +29,44 @@ import (
 	"sync"
 	"time"
 
+	"cloud.google.com/go/storage"
+	"github.com/golang/glog"
 	"github.com/google/cloudprober/logger"
 	"github.com/google/cloudprober/metrics"
 	"github.com/google/cloudprober/probes/options"
 	"github.com/google/cloudprober/targets/endpoint"
+	"google.golang.org/api/iterator"
 
-	"cloud.google.com/go/storage"
+	delete_pb "github.com/googleinterns/step224-2020/gcs/deleteFile/proto"
 )
 
 // Probe holds aggregate information about all probe runs, per-target.
 type Probe struct {
 	name    string
-	config  *ProbeConf
+	config  *delete_pb.DeleteFileProbe
 	targets []string
 	opts    *options.Options
 
-	res map[string]*metrics.EventMetrics // Results by target
-	l   *logger.Logger
+	probeOpLatency map[string]*metrics.EventMetrics           // Probe Operation Latency per target
+	apiCallLatency map[string]map[string]*metric.EventMetrics // Latency per API call per target (map[target]map[apiCall]metric).
+	logger         *logger.Logger
+
+	// Add other required fields here - bucket
+	// Array of Cancel funcs so that we can cancel externally, or just the context passed.
 }
 
 // Init initializes the probe with the given params.
 func (p *Probe) Init(name string, opts *options.Options) error {
-	c, ok := opts.ProbeConf.(*ProbeConf)
+	c, ok := opts.ProbeConf.(*delete_pb.DeleteFileProbe)
 	if !ok {
 		return fmt.Errorf("not a gcs.deleteFile config")
 	}
 	p.config = c
 	p.name = name
 	p.opts = opts
-	p.l = opts.Logger
-	p.res = make(map[string]*metrics.EventMetrics)
+	p.logger = opts.Logger
+	p.probeOpLatency = make(map[string]*metrics.EventMetrics)
+	p.apiCallLatency = make(map[string]map[string]*metrics.EventMetrics)
 
 	return nil
 }
@@ -74,8 +82,14 @@ func (p *Probe) Start(ctx context.Context, dataChan chan *metrics.EventMetrics) 
 			return
 		case <-probeTicker.C:
 			// On probe tick, write data to the channel and run probe.
-			for _, metric := range p.res {
+			for _, metric := range p.probeOpLatency {
 				dataChan <- metric
+			}
+
+			for _, apiCall := range p.apiCallLatency {
+				for _, metric := range apiCall {
+					dataChan <- metric
+				}
 			}
 
 			// TODO: CHANGE THIS BIT TO OUR PROBE IMPLEMENTATION
@@ -93,22 +107,40 @@ func (p *Probe) Start(ctx context.Context, dataChan chan *metrics.EventMetrics) 
 // initProbeMetrics initializes missing probe metrics.
 func (p *Probe) initProbeMetrics() {
 	for _, target := range p.targets {
-		if p.res[target] != nil {
-			continue
+		if p.probeOpLatency[target] == nil {
+
+			var probeLatDist *metrics.Distribution
+			probeLatDistPb := p.config.HermesProbeDef.GetProbeLatencyDistribution()
+			if probeLatDist, err = metrics.NewDistributionFromProto(probeLatDistPb); err != nil {
+				p.logger.Errorf("error creating probe latency distribution from the specification (%v): %v", probeLatDistPb, err)
+			}
+
+			p.probeOpLatency[target] = metrics.NewEventMetrics(time.Now()).
+				AddMetric("hermes_probe_operation_latency_s", probeLatDist).
+				AddLabel("storage_system", p.config.GetTarmgetSystem().String()).
+				AddLabel("probe_operation_type", p.config.GetProbeType().String()).
+				AddLabel("target", target)
 		}
-		var latVal metrics.Value
-		if p.opts.LatencyDist != nil {
-			latVal = p.opts.LatencyDist.Clone()
-		} else {
-			latVal = metrics.NewFloat(0)
+
+		if p.apiCallLatency[target] == nil {
+			p.apiCallLatency[target] = make(map[string]*metric.EventMetrics)
+
+			var apiCallLatDist *metrics.Distribution
+			apiCallLatDistPb := p.config.HermesProbeDef.GetApiCallLatencyDistribution()
+			if probeLatDist, err = metrics.NewDistributionFromProto(apiCallLatDistPb); err != nil {
+				p.logger.Errorf("error creating probe latency distribution from the specification (%v): %v", apiCallLatDistPb, err)
+			}
+
+			// List, Delete
+			apiCallList := []string{"list", "delete"}
+			for apiCall := range apiCallList {
+				p.apiCallLatDist[target][apiCall] = metrics.NewEventMetrics(time.Now()).
+					AddMetric("hermes_api_call_latency_ms", apiCallLatDist.Clone()).
+					AddLabel("storage_system", p.config.GetTargetSystem().String()).
+					AddLabel("api_call_type", apiCall).
+					AddLabel("target", target)
+			}
 		}
-		p.res[target] = metrics.NewEventMetrics(time.Now()).
-			AddMetric("total", metrics.NewInt(0)).
-			AddMetric("success", metrics.NewInt(0)).
-			AddMetric("latency", latVal).
-			AddLabel("ptype", "redis").
-			AddLabel("probe", p.name).
-			AddLabel("dst", target)
 	}
 }
 
@@ -120,7 +152,7 @@ func (p *Probe) runProbeForTarget(ctx context.Context, target string) error {
 	// Algorithm
 	// List objects, pick one, delete it.
 	// Check for errors (does GCS throw them?) -- YES.
-	bucket := p.c.GetBucket()
+	bucket := p.config.GetBucketName()
 
 	client, err := storage.NewClient(ctx)
 	if err != nil {
@@ -130,26 +162,37 @@ func (p *Probe) runProbeForTarget(ctx context.Context, target string) error {
 
 	// First, list objects
 	var names []string
-	iterator := bkt.Objects(ctx, &storage.Query{Prefix: ""})
+
+	start := time.Now()
+	it := client.Bucket(bucket).Objects(ctx, &storage.Query{Prefix: ""})
+	p.apiCallLatDist[target]["list"].AddFloat64(time.Now().Sub(start)) // What unit???
+
 	for {
 		attrs, err := it.Next()
 		if err == iterator.Done {
 			break
 		}
 		if err != nil {
-			p.logger.Error(err)
+			p.logger.Errorf("%v", err)
 		}
 		names = append(names, attrs.Name)
 	}
 
 	// Then, pick one to delete.
+	object := names[0]
 
+	// Then delete it
 	o := client.Bucket(bucket).Object(object)
+	start = time.Now()
 	if err := o.Delete(ctx); err != nil {
+		p.apiCallLatDist[target]["delete"].AddFloat64(time.Now().Sub(start))     // TODO(evanSpendlove) Add unit
+		p.apiCallLatDist[target]["delete"].AddLabel("exit_status", err.String()) // TODO(evanSpendlove) Add a function that turns this into a proper label value
 		return fmt.Errorf("Object(%q).Delete: %v", object, err)
 	}
+	p.apiCallLatDist[target]["delete"].AddFloat64(time.Now().Sub(start))
+	p.apiCallLatdist[target]["delete"].AddLabel("exit_status", "success")
 
-	glog.Infof("Object %v deleted in bucket %s.", o, bucket)
+	p.logger.Infof("Object %v deleted in bucket %s.", o, bucket)
 	return nil
 }
 
@@ -167,12 +210,12 @@ func (p *Probe) runProbe(ctx context.Context) {
 			start := time.Now()
 			err := p.runProbeForTarget(ctx, target) // run probe just for a single target
 			if err != nil {
-				p.l.Errorf(err.Error())
+				p.logger.Errorf(err.Error())
 				return
 			}
 			em.Metric("success").AddInt64(1)
 			em.Metric("latency").AddFloat64(time.Now().Sub(start).Seconds() / p.opts.LatencyUnit.Seconds())
-		}(target, p.res[target])
+		}(target, p.results[target])
 
 	}
 
