da3ba77b12234710671468b668fc58a08913094b
alicjakwie <59053998+alicjakwie@users.noreply.github.com>
2020-09-18 08:13:05 +0200

    Update and rename create_file_test.go to create_test.go

--- hermes/probe/create/create_test.go
+++ hermes/probe/create/create_test.go
@@ -1,6 +1,18 @@
-// TODO (#70) add license header and author line
-// TODO (#68) add doc strings
-// TODO (#72) change error types and error messages
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: Alicja Kwiecinska GitHub: alicjakwie
+//
+// TODO (#72) change error types to be compatible with ProbeError
 
 package create
 
@@ -21,28 +33,28 @@ import (
 
 func TestNewRandomFile(t *testing.T) {
 	tests := []struct {
-		fileID   int64
+		fileID   int32
 		fileSize int
+		want   *randomFile
 		wantErr  bool
-		wantRF   *RandomFile
 	}{
-		{51, 12, true, &RandomFile{}},
-		{0, 50, true, &RandomFile{}},
-		{3, 100, false, &RandomFile{3, 100}},
-		{12, 100, false, &RandomFile{12, 100}},
-		{3, 0, true, &RandomFile{}},
-		{3, 1001, true, &RandomFile{}},
+		{51, 12, &randomFile{}, true},
+		{0, 50, &randomFile{}, true},
+		{3, 100, &randomFile{3, 100}, false},
+		{12, 100, &randomFile{12, 100}, false},
+		{3, 0, &randomFile{}, false},
+		{3, 1001, &randomFile{}, false},
 	}
 	for _, test := range tests {
-		got, err := NewRandomFile(test.fileID, test.fileSize)
+		got, err := newRandomFile(test.fileID, test.fileSize)
 		if err != nil && !test.wantErr {
-			t.Errorf("{%v, %v}.NewRandomFile() failed and returned an unexpected error %s", test.fileID, test.fileSize, err.Error())
+			t.Errorf("{%d, %d}.newRandomFile() failed and returned an unexpected error %v", test.fileID, test.fileSize, err)
 		}
 		if err == nil && test.wantErr {
-			t.Errorf("{%v, %v}.NewRandomFile() failed expected an error got nil", test.fileID, test.fileSize)
+			t.Errorf("{%d, %d}.newRandomFile() failed expected an error got nil", test.fileID, test.fileSize)
 		}
-		if got.ID != test.wantRF.ID || got.Size != test.wantRF.Size {
-			t.Errorf("{%v, %v}.NewRandomFile() failed expected {%v,%v}, got {%v,%v}", test.fileID, test.fileSize, test.wantRF.ID, test.wantRF.Size, got.ID, got.Size)
+		if got.id != test.wantRF.id || got.size != test.wantRF.size {
+			t.Errorf("{%d, %d}.newRandomFile() failed expected {%d,%d}, got {%d,%d}", test.fileID, test.fileSize, test.wantRF.id, test.wantRF.size, got.id, got.size)
 		}
 
 	}
@@ -51,47 +63,47 @@ func TestNewRandomFile(t *testing.T) {
 
 func TestFileName(t *testing.T) {
 	tests := []struct {
-		file     *RandomFile
+		file     *randomFile
 		wantName string
 	}{
-		{&RandomFile{3, 100}, "Hermes_03"},
-		{&RandomFile{12, 100}, "Hermes_12"},
-		{&RandomFile{9, 20}, "Hermes_09"},
+		{&randomFile{3, 100}, "Hermes_03"},
+		{&randomFile{12, 100}, "Hermes_12"},
+		{&randomFile{8, 20}, "Hermes_08"},
 	}
 
 	for _, test := range tests {
-		got, err := test.file.FileName()
+		got, err := test.file.fileName()
 		if err != nil {
-			t.Errorf("{%v, %v}.FileName() failed and returned an unexpected error %s", test.file.ID, test.file.Size, err.Error())
+			t.Errorf("{%d, %d}.fileName() failed and returned an unexpected error %v", test.file.id, test.file.size, err)
 		}
-		if got[0:9] != test.wantName {
-			t.Errorf("{%v, %v}.FileName() failed expected prefix %s got prefix %s", test.file.ID, test.file.Size, test.wantName, got[0:9])
+		if got[:FileNamePrefixSize] != test.wantName {
+			t.Errorf("{%d, %d}.fileName() failed expected prefix %q got prefix %q", test.file.id, test.file.size, test.wantName, got[:FileNamePrefixSize])
 		}
 	}
 }
 
 func TestChecksum(t *testing.T) {
-	file := RandomFile{11, 100}
-	checksum, err := file.CheckSum()
+	file := randomFile{11, 100}
+	checksum, err := file.checksum()
 	if err != nil {
 		t.Error(err)
 	}
-	file = RandomFile{13, 100}
-	otherChecksum, err := file.CheckSum()
+	file = randomFile{13, 100}
+	otherChecksum, err := file.checksum()
 	if err != nil {
 		t.Error(err)
 	}
 	if fmt.Sprintf("%x", checksum) == fmt.Sprintf("%x", otherChecksum) {
-		t.Errorf("Checksum returned the same value for two different RandomFiles {%v, %v} and {%v, %v}", file.ID, file.Size, file.ID, file.Size)
+		t.Errorf("Checksum returned the same value for two different randomFiles {%d, %d} and {%d, %d}", file.id, file.size, file.id, file.size)
 
 	}
-	file = RandomFile{11, 100}
-	checksumAgain, err := file.CheckSum()
+	file = randomFile{11, 100}
+	checksumAgain, err := file.checksum()
 	if err != nil {
 		t.Error(err)
 	}
 	if fmt.Sprintf("%x", checksum) != fmt.Sprintf("%x", checksumAgain) {
-		t.Errorf("Checksum returned different values for the same RandomFiles {%v, %v}", file.ID, file.Size)
+		t.Errorf("Checksum returned different values for the same randomFiles {%d, %d}", file.id, file.size)
 	}
 }
 
@@ -99,8 +111,6 @@ func TestCreateFile(t *testing.T) {
 	ctx := context.Background()
 	bucketName := "test_bucket_probe0"
 	client := fakegcs.NewClient()
-	fbh := client.Bucket(bucketName)                         // fakeBucketHandle
-	if err := fbh.Create(ctx, bucketName, nil); err != nil {  // creates the bucket with name "test_bucket_probe0"
 		t.Error(err)
 	}
 	fileID := int32(6)
@@ -150,11 +160,10 @@ func TestCreateFile(t *testing.T) {
 
 	var err error
 	if target.LatencyMetrics, err = metrics.NewMetrics(hp, probeTarget); err != nil {
-		t.Fatalf("Metric set up failed due to %s", err.Error())
+		t.Fatalf("metrics.NewMetrics(): %v", err)
 	}
-
-	if err := CreateFile(ctx, target, fileID, fileSize, client, nil); err != nil {
+	logger := fakegcs.NewLogger(ctx).Logger
+	if err := CreateFile(ctx, target, fileID, fileSize, client, logger); err != nil {
 		t.Error(err)
 	}
 }
-
